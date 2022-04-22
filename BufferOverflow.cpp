3f29c50c759ddf1944ea56b1f40adef5045df47e
Clara Kellermann-Bryant <78874917+GalarianRapidash2345@users.noreply.github.com>
2022-04-22 15:43:03 -0600

    Update BufferOverflow.cpp

--- BufferOverflow.cpp
+++ BufferOverflow.cpp
@@ -1,28 +1,11 @@
-// BufferOverflow.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// These are snippets of my work
 
 #include <iomanip>
 #include <iostream>
 
 int main()
 {
-  std::cout << "Buffer Overflow Example" << std::endl;
-
-  // TODO: The user can type more than 20 characters and overflow the buffer, resulting in account_number being replaced -
-  //  even though it is a constant and the compiler buffer overflow checks are on.
-  //  You need to modify this method to prevent buffer overflow without changing the account_order
-  //  variable, and its position in the declaration. It must always be directly before the variable used for input.
-
   
-
-
-  const std::string account_number = "CharlieBrown42";
-  char user_input[20];
-  std::cout << "Enter a value: ";
-  std::cin >> user_input;
-
-  std::cout << "You entered: " << user_input << std::endl;
-  std::cout << "Account Number = " << account_number << std::endl;
 
 
   // This will prevent buffer overflows without changing the account_order or account_number variables, and without changing the 
